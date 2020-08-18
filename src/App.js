9ca3f808d19e763ca8397c20472684c56f225733
Nathan Luu <nate@Nathans-MBP.hsd1.ca.comcast.net>
2020-08-18 15:16:02 -0700

    created project preview component and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
+import PreviewProjects from "./PreviewProjects.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello. My name is Nathan Luu. I am full stack developer and I love what
-        I do.
-      </h1>
+      <PreviewProjects />
+      <h1>Hi, I am Nathan . I am full stack developer and I love what I do.</h1>
     </div>
   );
 }

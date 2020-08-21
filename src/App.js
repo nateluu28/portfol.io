74c1e906ed6dd231c4a0272edd67c64d00d13448
Nathan Luu <nateluu28@gmail.com>
2020-08-20 22:06:11 -0700

    added react routing to project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { PreviewProjects } from "./PreviewProjects";
 import { TopBar } from "./TopBar";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, Switch, Route, Router } from "react-router";
 
 function App() {
   return (
     <div className="App">
-      <TopBar />
-      <h1>
-        Hi, I am Nathan . I am full stack developer and I love what I do..
-      </h1>
-      <PreviewProjects />
+      <Router>
+        <TopBar />
+        <h1>
+          Hi, I am Nathan . I am full stack developer and I love what I do..
+        </h1>
+        <PreviewProjects />
+      </Router>
     </div>
   );
 }

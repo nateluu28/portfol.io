e9330af65f02373fcbb22fd9c9f35bcebae3c5d8
Nathan Luu <nateluu28@gmail.com>
2020-10-01 16:30:15 -0700

    Working fix on the landing pages and about pages

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { PreviewProjects } from "./PreviewProjects";
 import { TopBar } from "./TopBar";
-// import { Link, Switch, Route, Router } from "react-router";
 import { ProjectCards } from "./ProjectCards";
+
+import About from "./About";
+import Experience from "./Experience";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Router> */}
-      <TopBar />
+      <Router>
+        <TopBar />
+        <Route path="/home" component={Experience} />
+        <Route path="/About" component={About} />
+      </Router>
+
       <h1>Hi, I am Nathan . I am full stack developer and I love what I do.</h1>
       <PreviewProjects />
       <ProjectCards />
-      {/* </Router> */}
     </div>
   );
 }
--- src/TopBar.js
+++ src/TopBar.js
@@ -5,12 +5,12 @@ import Nav from "react-bootstrap/Nav";
 export const TopBar = () => {
   return (
     <Navbar bg="light">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Nathan Luu</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="about">Home</Nav.Link>
+          <Nav.Link href="experience">Link</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>

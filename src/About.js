78e367771524a051478771dfc608c13989205238
Nathan Luu <nateluu28@gmail.com>
2020-10-07 04:17:40 -0700

    added react-tooltip to the icons

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import {
   faPython,
   faJava,
@@ -8,6 +8,12 @@ import {
   faReact,
   faGitAlt,
 } from "@fortawesome/free-brands-svg-icons";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+const renderTooltip = (props) => (
+  <Tooltip {...props}>Tooltip for the register button</Tooltip>
+);
 
 function About() {
   return (
@@ -16,12 +22,18 @@ function About() {
         Hi, I'm Nathan Luu. I have a passion for coding! I am a full-stack
         developer with experience in Python, Javascript, and Java.
       </h1>
-      <h2>Expert: </h2>
-      <FontAwesomeIcon icon={faPython} size="6x" />
-      <FontAwesomeIcon icon={faJava} size="6x" />
-      <FontAwesomeIcon icon={faJsSquare} size="6x" />
-      <FontAwesomeIcon icon={faReact} size="6x" />
-      <FontAwesomeIcon icon={faGitAlt} size="6x" />
+      <div className="icons">
+        <h2>
+          Expert:
+          <OverlayTrigger placement="top" overlay={renderTooltip}>
+            <FontAwesomeIcon icon={faPython} size="lg" />
+          </OverlayTrigger>
+          <FontAwesomeIcon icon={faJava} size="lg" />
+          <FontAwesomeIcon icon={faJsSquare} size="lg" />
+          <FontAwesomeIcon icon={faReact} size="lg" />
+          <FontAwesomeIcon icon={faGitAlt} size="lg" />
+        </h2>
+      </div>
     </div>
   );
 }
